5e57e810e7586a9a99cc1a2f450a02ce2c8f0bda
hrajpal <hrajpal@ebrevia.com>
2020-12-06 22:17:42 -0500

    setting orm

---
+++ config/orm.js
@@ -0,0 +1,17 @@
+var connection = require('../config/connection.js');
+
+
+var orm = {
+
+    selectAll: function (cb) {
+        var queryString = "SELECT * FROM burgers";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+
+module.exports = orm;
